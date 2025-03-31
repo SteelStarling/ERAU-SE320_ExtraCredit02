542ee2b7ccd6e9fac621dca353b1ca029e11bea9
Taylor Hancock <58285541+SteelStarling@users.noreply.github.com>
2025-03-30 23:44:34 -0700

    Initial commit, added README, .gitignore, and LICENSE

---
+++ secret_message.py
@@ -0,0 +1,13 @@
+"""Code to decode a "Secret Message" for SE320
+Author: Taylor Hancock
+Date:   03/30/2025
+Class:  SE320 - Software Construction
+Assignment: Decode a Secret Message - Extra Credit
+"""
+
+
+TEST_URL = https://docs.google.com/document/d/e/2PACX-1vRMx5YQlZNa3ra8dYYxmv-QIQ3YJe8tbI3kqcuC7lQiZm-CSEznKfN_HYNSpoXcZIV3Y_O3YoUB1ecq/pub
+
+def url_to_grid(url = TEST_URL):
+    """Prints a grid of characters based on a URL's contents"""
+    pass
